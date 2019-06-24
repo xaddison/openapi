c06338d002313ebbb51fddd40f4aa91f72cb2027
pacocoursey <pvco.coursey@gmail.com>
2019-06-24 12:42:28 -0700

    Improve header styling, add logo

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -8,6 +8,20 @@ type AugmentingLayoutP = {
   getComponent(comp: string, nose: boolean): any
 }
 
+const Logo = () => (
+  <svg width="30" viewBox="0 0 226 200" aria-label="zeit" className="logo">
+    <defs>
+      <linearGradient x1="196.572434%" y1="228.815483%" x2="50%" y2="50%" id="logo-d-zeit">
+        <stop offset="0%" stopColor="#fff" />
+        <stop offset="100%" stopColor="#000" />
+      </linearGradient>
+    </defs>
+    <g transform="translate(-141, -156)">
+      <polygon fill="url(#logo-d-zeit)" points="254 156.459299 367 356 141 356 " />
+    </g>
+  </svg>
+)
+
 class AugmentingLayout extends Component<AugmentingLayoutP, {}> {
   render() {
     const { getComponent } = this.props;
@@ -16,6 +30,9 @@ class AugmentingLayout extends Component<AugmentingLayoutP, {}> {
     return (
       <div>
         <div className="custom-header">
+          <a href="https://zeit.co">
+            <Logo />
+          </a>
           <h1>ZEIT OpenAPI Spec</h1>
         </div>
         <BaseLayout />
